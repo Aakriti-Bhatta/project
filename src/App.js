ccfa9897be737eea8383df4d2bf17b7a04df9fe4
Aakriti <bhatta.aakriti81@gmail.com>
2022-08-06 18:26:13 -0400

    header comp added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
 function App() {
   return (
+    <>
+    <Header/>
+
    <Container>
     <h1>hello World</h1>
    </Container>
+    </>
   );
 }
 
