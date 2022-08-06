32b99a17788825f57061cfaa6afe580b23b3a4e8
Aakriti <bhatta.aakriti81@gmail.com>
2022-08-05 22:09:56 -0400

    container component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 
-
+import { Container } from "./components/styles/Container.styled";
 function App() {
   return (
-    <div className="App">
-    <h1>Hello World</h1>
-     
-    </div>
+   <Container>
+    <h1>hello World</h1>
+   </Container>
   );
 }
 
