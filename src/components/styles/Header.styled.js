e2273849a53ddafe4a326c8626e88157ade748a2
Aakriti <bhatta.aakriti81@gmail.com>
2022-08-08 13:42:02 -0400

    styled Nav component

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -4,7 +4,15 @@
   background-color: ${({theme})=>theme.colors.header};
   padding:40px 0;
   
-  
-  
-  
+  `
+
+
+  export const Nav = styled.nav`
+   display:flex;
+   align-items:ceneter;
+   justify-content: space-between;
+   margin-bottom: 40px;
+  `
+
+  export const Logo=styled.img`
   `
