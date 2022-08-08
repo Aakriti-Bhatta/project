cb192b42bbfc4df00591b75b7fb569c3a5a530eb
Aakriti <bhatta.aakriti81@gmail.com>
2022-08-08 13:43:05 -0400

    header created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
-import { StyledHeader } from "./styles/Header.styled";
-
+import { Container } from "./styles/Container.styled";
+import { StyledHeader, Nav,Logo } from "./styles/Header.styled";
+import { Button } from "./styles/Button.styles";
 
 export default function Header() {
   return (
     <StyledHeader>
+    <Container>
+    <Nav>
+      <Logo src="./images/logo.svg" alt=""/>
+      <Button>Try It Free</Button>
+    </Nav>
+
         <h1>Hubble</h1>
+    </Container>
     </StyledHeader>
   )
 }
